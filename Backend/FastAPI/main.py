9450b1a3300064611ab8060fc0127f60edea6735
GallegosC <jdanielgch@hotmail.com>
2026-04-03 01:11:20 -0500

    docs: agregar notas de uvicorn

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,13 +1,18 @@
+#Instala FastAPI: pip install "fastapi[standard]"
 from fastapi import FastAPI
 
 app  = FastAPI() 
 
 
+#Url local: https://127.0.0.1.8000
 @app.get("/")
 async def root():
     return "Hola Fastapi"
 
+#Url local: https://127.0.0.1.8000/url
 @app.get("/url")
 async def url():
     return { "url_curso":"https://mouredev.com/python" }
 
+#Inicia Server: uvicorn main:app --reload
+#Detener Server: Ctrl + C
